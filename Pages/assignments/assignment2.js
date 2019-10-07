62ab444e79eb3a94149c4e81a74af09adc6989e3
Subi6224 <868472@pdsb.net>
2019-10-07 08:50:48 -0400

    Update assignment2.js

--- Pages/assignments/assignment2.js
+++ Pages/assignments/assignment2.js
@@ -15,6 +15,6 @@ function angles(){
 	//var usernum=prompt("Give me 2 angles, and I'll tell you the last angle.");
 	var angle1 = window.prompt("Give me 2 angles, and I'll tell you the last angle.");
 	var angle2 = window.prompt("Give me 2 angles, and I'll tell you the last angle.");
-	alert(180-angle1+angle2);
+	alert(180-(angle1+angle2);
 }
 
