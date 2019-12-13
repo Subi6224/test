127a1e300eb21f451f39982d0a5d4714473a580d
Subi6224 <868472@pdsb.net>
2019-12-13 09:22:52 -0500

    Update assignment6.js

--- Pages/assignments/6/assignment6.js
+++ Pages/assignments/6/assignment6.js
@@ -1,15 +1,95 @@
 function encryption(usercode){
-  let usercode="Boo! It's a secret message!";
-  let usercode=usercode.split(/(\W+)/);
+  usercode="Boo! It's a secret message!";
+  usercode=usercode.split(/(\W+)/);
   
-//  for (i = 0; i < usercode.length; i++){
-//    if usercode[i].length>2{
-//      usercode.concat("djkvs".scramble);
-      
-//    }
-//  }
+  for (i = 0; i < usercode.length; i++){
+    if usercode[i]%2==0{
+    	switch(x) {
+	case "a":
+	return "f";
+	break;
+  		case "b":
+  		return "g";
+    	break;
+	case "c":
+	return "h";
+    break;
+  		case "d":
+  		return "i";
+    	break;
+  	case "e":
+  	return "j";
+    break;
+      	case "f":
+  		return "k";
+    	break;
+    case "g":
+	return "l";
+    break;
+      	case "h":
+  		return "m";
+    	break;
+    case "i":
+  	return "n";
+    break;
+  		case "j":
+  		return "o";
+    	break;
+    case "k":
+	return "p";
+    break;
+      	case "l":
+  		return "q";
+    	break;
+    case "m":
+  	return "r";
+    break;
+      	case "n":
+  		return "s";
+    	break;
+    case "o":
+  	return "t";
+    break;
+      	case "p":
+  		return "u";
+    	break;
+    case "q":
+  	return "v";
+    break;
+      	case "r":
+  		return "w";
+    	break;
+    case "s":
+  	return "x";
+    break;
+      	case "t":
+  		return "y";
+    	break;
+    case "u":
+  	return "z";
+    break;
+      	case "v":
+  		return "a";
+    	break;
+    case "w":
+  	return "b";
+    break;
+      	case "x":
+  		return "c";
+    	break;
+    case "y":
+  	return "d";
+    break;
+      	case "z":
+  		return "e";
+    	break;
+  default:
+    }
 
-switch() {
+
+
+    else{
+    	switch(x) {
 	case "a":
 	return "b";
 	break;
@@ -89,7 +169,10 @@ switch() {
   		return "a";
     	break;
   default:
-    // code block
-}
+    }
+  }
+
+
+ }
   console.log usercode;
 }
