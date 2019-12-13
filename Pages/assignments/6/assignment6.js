2505a77395645111928efb24744f508086c87fb5
Subi6224 <868472@pdsb.net>
2019-12-13 07:18:31 -0500

    Update assignment6.js

--- Pages/assignments/6/assignment6.js
+++ Pages/assignments/6/assignment6.js
@@ -2,11 +2,94 @@ function encryption(){
   let usercode="Boo! It's a secret message!";
   let usercode=usercode.split(/(\W+)/);
   
-  for (i = 0; i < usercode.length; i++){
-    if usercode[i].length>2{
-      usercode.concat("djkvs".scramble);
+//  for (i = 0; i < usercode.length; i++){
+//    if usercode[i].length>2{
+//      usercode.concat("djkvs".scramble);
       
-    }
-  }
+//    }
+//  }
+
+switch() {
+	case "a":
+	return "b";
+	break;
+  		case "b":
+  		return "c";
+    	break;
+	case "c":
+	return "d";
+    break;
+  		case "d":
+  		return "e";
+    	break;
+  	case "e":
+  	return "f";
+    break;
+      	case "f":
+  		return "g";
+    	break;
+    case "g":
+	return "h";
+    break;
+      	case "h":
+  		return "i";
+    	break;
+    case "i":
+  	return "j";
+    break;
+  		case "j":
+  		return "k";
+    	break;
+    case "k":
+	return "l";
+    break;
+      	case "l":
+  		return "m";
+    	break;
+    case "m":
+  	return "n";
+    break;
+      	case "n":
+  		return "o";
+    	break;
+    case "o":
+  	return "p";
+    break;
+      	case "p":
+  		return "q";
+    	break;
+    case "q":
+  	return "r";
+    break;
+      	case "r":
+  		return "s";
+    	break;
+    case "s":
+  	return "t";
+    break;
+      	case "t":
+  		return "u";
+    	break;
+    case "u":
+  	return "v";
+    break;
+      	case "v":
+  		return "w";
+    	break;
+    case "w":
+  	return "x";
+    break;
+      	case "x":
+  		return "y";
+    	break;
+    case "y":
+  	return "z";
+    break;
+      	case "z":
+  		return "a";
+    	break;
+  default:
+    // code block
+}
   console.log usercode;
 }
