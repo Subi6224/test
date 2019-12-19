473038ba1d5a237d9897755e62a242fd3acabee5
Subi6224 <868472@pdsb.net>
2019-12-19 08:42:19 -0500

    6

--- Pages/assignments/6/assignment6.js
+++ Pages/assignments/6/assignment6.js
@@ -3,186 +3,186 @@ function e(){
   	usercode=usercode.split(/(\W+)/);
   
   for (i = 0; i < usercode.length; i++){
-    if usercode[i]%2==0{
-    switch(x) {
-		case "a":
-		return "f";
-		break;
-	  		case "b":
-	  		return "g";
-	    	break;
-		case "c":
-		return "h";
-	    break;
-	  		case "d":
-	  		return "i";
-	    	break;
-	  	case "e":
-	  	return "j";
-	    break;
-	      	case "f":
-	  		return "k";
-	    	break;
-	    case "g":
-		return "l";
-	    break;
-	      	case "h":
-	  		return "m";
-	    	break;
-	    case "i":
-	  	return "n";
-	    break;
-	  		case "j":
-	  		return "o";
-	    	break;
-	    case "k":
-		return "p";
-	    break;
-	      	case "l":
-	  		return "q";
-	    	break;
-	    case "m":
-	  	return "r";
-	    break;
-	      	case "n":
-	  		return "s";
-	    	break;
-	    case "o":
-	  	return "t";
-	    break;
-	      	case "p":
-	  		return "u";
-	    	break;
-	    case "q":
-	  	return "v";
-	    break;
-	      	case "r":
-	  		return "w";
-	    	break;
-	    case "s":
-	  	return "x";
-	    break;
-	      	case "t":
-	  		return "y";
-	    	break;
-	    case "u":
-	  	return "z";
-	    break;
-	      	case "v":
-	  		return "a";
-	    	break;
-	    case "w":
-	  	return "b";
-	    break;
-	      	case "x":
-	  		return "c";
-	    	break;
-	    case "y":
-	  	return "d";
-	    break;
-	      	case "z":
-	  		return "e";
-	    	break;
-	  default:
+    if (usercode[i]%2==0){
+	    switch(x) {
+			case "a":
+			return "f";
+			break;
+		  		case "b":
+		  		return "g";
+		    	break;
+			case "c":
+			return "h";
+		    break;
+		  		case "d":
+		  		return "i";
+		    	break;
+		  	case "e":
+		  	return "j";
+		    break;
+		      	case "f":
+		  		return "k";
+		    	break;
+		    case "g":
+			return "l";
+		    break;
+		      	case "h":
+		  		return "m";
+		    	break;
+		    case "i":
+		  	return "n";
+		    break;
+		  		case "j":
+		  		return "o";
+		    	break;
+		    case "k":
+			return "p";
+		    break;
+		      	case "l":
+		  		return "q";
+		    	break;
+		    case "m":
+		  	return "r";
+		    break;
+		      	case "n":
+		  		return "s";
+		    	break;
+		    case "o":
+		  	return "t";
+		    break;
+		      	case "p":
+		  		return "u";
+		    	break;
+		    case "q":
+		  	return "v";
+		    break;
+		      	case "r":
+		  		return "w";
+		    	break;
+		    case "s":
+		  	return "x";
+		    break;
+		      	case "t":
+		  		return "y";
+		    	break;
+		    case "u":
+		  	return "z";
+		    break;
+		      	case "v":
+		  		return "a";
+		    	break;
+		    case "w":
+		  	return "b";
+		    break;
+		      	case "x":
+		  		return "c";
+		    	break;
+		    case "y":
+		  	return "d";
+		    break;
+		      	case "z":
+		  		return "e";
+		    	break;
+		  default:
     }
 
 
 
 	else{
-    switch(x) {
-		case "a":
-		return "b";
-		break;
-	  		case "b":
-	  		return "c";
-	    	break;
-		case "c":
-		return "d";
-	    break;
-	  		case "d":
-	  		return "e";
-	    	break;
-	  	case "e":
-	  	return "f";
-	    break;
-	      	case "f":
-	  		return "g";
-	    	break;
-	    case "g":
-		return "h";
-	    break;
-	      	case "h":
-	  		return "i";
-	    	break;
-	    case "i":
-	  	return "j";
-	    break;
-	  		case "j":
-	  		return "k";
-	    	break;
-	    case "k":
-		return "l";
-	    break;
-	      	case "l":
-	  		return "m";
-	    	break;
-	    case "m":
-	  	return "n";
-	    break;
-	      	case "n":
-	  		return "o";
-	    	break;
-	    case "o":
-	  	return "p";
-	    break;
-	      	case "p":
-	  		return "q";
-	    	break;
-	    case "q":
-	  	return "r";
-	    break;
-	      	case "r":
-	  		return "s";
-	    	break;
-	    case "s":
-	  	return "t";
-	    break;
-	      	case "t":
-	  		return "u";
-	    	break;
-	    case "u":
-	  	return "v";
-	    break;
-	      	case "v":
-	  		return "w";
-	    	break;
-	    case "w":
-	  	return "x";
-	    break;
-	      	case "x":
-	  		return "y";
-	    	break;
-	    case "y":
-	  	return "z";
-	    break;
-	      	case "z":
-	  		return "a";
-	    	break;
-	  default:
+	    switch(x) {
+			case "a":
+			return "b";
+			break;
+		  		case "b":
+		  		return "c";
+		    	break;
+			case "c":
+			return "d";
+		    break;
+		  		case "d":
+		  		return "e";
+		    	break;
+		  	case "e":
+		  	return "f";
+		    break;
+		      	case "f":
+		  		return "g";
+		    	break;
+		    case "g":
+			return "h";
+		    break;
+		      	case "h":
+		  		return "i";
+		    	break;
+		    case "i":
+		  	return "j";
+		    break;
+		  		case "j":
+		  		return "k";
+		    	break;
+		    case "k":
+			return "l";
+		    break;
+		      	case "l":
+		  		return "m";
+		    	break;
+		    case "m":
+		  	return "n";
+		    break;
+		      	case "n":
+		  		return "o";
+		    	break;
+		    case "o":
+		  	return "p";
+		    break;
+		      	case "p":
+		  		return "q";
+		    	break;
+		    case "q":
+		  	return "r";
+		    break;
+		      	case "r":
+		  		return "s";
+		    	break;
+		    case "s":
+		  	return "t";
+		    break;
+		      	case "t":
+		  		return "u";
+		    	break;
+		    case "u":
+		  	return "v";
+		    break;
+		      	case "v":
+		  		return "w";
+		    	break;
+		    case "w":
+		  	return "x";
+		    break;
+		      	case "x":
+		  		return "y";
+		    	break;
+		    case "y":
+		  	return "z";
+		    break;
+		      	case "z":
+		  		return "a";
+		    	break;
+		  default:
     }
   }
 
 
  }
-  console.log usercode;
+ alert(usercode)
 }
 
 function d(){
-	let usercode=prompt("Give me a code, and I'll edecrypt it for you.");
+	let usercode=prompt("Give me a code, and I'll decrypt it for you.");
   	usercode=usercode.split(/(\W+)/);
   
   for (i = 0; i < usercode.length; i++){
-    if usercode[i]%2==0{
+    if (usercode[i]%2==0){
     switch(x) {
 		case "f":
 		return "a";
@@ -353,5 +353,5 @@ function d(){
 
 
  }
-  console.log usercode;
+ 	alert(usercode)
 }
